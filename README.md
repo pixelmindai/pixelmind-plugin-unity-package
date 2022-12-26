4959496e05171477888b892fa705bb412abfb6b4
despotic <geoligard@gmail.com>
2022-12-26 14:27:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Pixelmind Generator
+
+Unity package that allow you to generate infinite game assets using AI.
+
+## Install
+
+Open `Packages/manifest.json` with your favourite editor and add the following in your `dependencies` (make sure to respect JSON commas):
+
+```json
+{
+ "dependencies": {
+  "com.pusher.pusherwebsocketunity": "https://github.com/pusher/pusher-websocket-unity.git#upm",
+  "ai.330.pixelmindgenerator": "https://github.com/pixelmindai/pixelmind-plugin-unity-package.git#develop"
+ }
+}
+```
+
+## Getting Started
+
+### Editor
+
+1. Select a game object
+2. Click 'Add Component' in the 'Inspector' window
+3. Type 'Pixelmind Generator' and add the component
+4. Click 'Get generators'
+5. Enter a texture description, for example 'plasma gun'
+6. If you don't want the sprite to be modified, uncheck 'Assign To Sprite Renderer'
+7. Click 'Generate'
+8. Optionally you can save your texture as a prefab now
+
+### Runtime
+
+1. Click on the 'Enable GUI' button
+2. Use the in-game form the same way as mentioned above in the editor section.
+
+
+
+
+
+
+
---
+++ Runtime/PixelmindGeneratorEditor.cs
@@ -0,0 +1,147 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PixelmindImaginarium
+{
+#if UNITY_EDITOR
+    [CustomEditor(typeof(PixelmindGenerator))]
+    public class PixelmindGeneratorEditor : Editor
+    {
+        private SerializedProperty assignToMaterial;
+        private SerializedProperty assignToSpriteRenderer;
+        private SerializedProperty enableGUI;
+        private SerializedProperty resultImage;
+        private SerializedProperty apiKey;
+        private SerializedProperty generatorFields;
+        private SerializedProperty generators;
+        private SerializedProperty generatorOptions;
+        private SerializedProperty generatorOptionsIndex;
+        private bool showApi = true;
+        private bool showBasic = true;
+        private bool showGenerators = true;
+        private bool showOutput = true;
+        private bool showGenerationButtons = true;
+        
+        void OnEnable()
+        {
+            assignToMaterial = serializedObject.FindProperty("assignToMaterial");
+            assignToSpriteRenderer = serializedObject.FindProperty("assignToSpriteRenderer");
+            enableGUI = serializedObject.FindProperty("enableGUI");
+            apiKey = serializedObject.FindProperty("apiKey");
+            resultImage = serializedObject.FindProperty("resultImage");
+            generatorFields = serializedObject.FindProperty("generatorFields");
+            generators = serializedObject.FindProperty("generators");
+            generatorOptions = serializedObject.FindProperty("generatorOptions");
+            generatorOptionsIndex = serializedObject.FindProperty("generatorOptionsIndex");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorUtility.SetDirty(target);
+
+            serializedObject.Update();
+
+            var pixelmindGenerator = (PixelmindGenerator)target;
+
+            showApi = EditorGUILayout.Foldout(showApi, "Api");
+
+            if (showApi)
+            {
+                EditorGUILayout.PropertyField(apiKey);
+            }
+
+            showBasic = EditorGUILayout.Foldout(showBasic, "Basic Settings");
+
+            if (showBasic)
+            {
+                EditorGUILayout.PropertyField(enableGUI);
+                EditorGUILayout.PropertyField(assignToSpriteRenderer);
+                EditorGUILayout.PropertyField(assignToMaterial);
+            }
+
+            if (!EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                showGenerators = EditorGUILayout.Foldout(showGenerators, "Generators");
+
+                if (showGenerators)
+                {
+                    if (GUILayout.Button("Get Generators"))
+                    {
+                        _ = pixelmindGenerator.GetGeneratorsWithFields();
+                    }
+
+                    // Iterate over generator fields and render them in the GUI
+                    if (pixelmindGenerator.generatorFields.Count > 0)
+                    {
+                        RenderEditorFields(pixelmindGenerator);
+                    }
+                }
+
+                showGenerationButtons = EditorGUILayout.Foldout(showGenerationButtons, "Generate");
+
+                if (showGenerationButtons)
+                {
+                    if (pixelmindGenerator.PercentageCompleted() >= 0 && pixelmindGenerator.PercentageCompleted() < 100)
+                    {
+                        if (GUILayout.Button("Cancel (" + pixelmindGenerator.PercentageCompleted() + "%)"))
+                        {
+                            pixelmindGenerator.Cancel();
+                        }
+                    }
+                    else
+                    {
+                        if (GUILayout.Button("Generate"))
+                        {
+                            _ = pixelmindGenerator.InitializeGeneration(
+                                pixelmindGenerator.generatorFields,
+                                pixelmindGenerator.generators[pixelmindGenerator.generatorOptionsIndex].generator
+                            );
+                        }
+                    }
+                }
+
+                showOutput = EditorGUILayout.Foldout(showOutput, "Output");
+
+                if (showOutput)
+                {
+                    EditorGUILayout.PropertyField(resultImage);
+                    if (pixelmindGenerator.previewImage != null) GUILayout.Box(pixelmindGenerator.previewImage);
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void RenderEditorFields(PixelmindGenerator pixelmindGenerator)
+        {
+            EditorGUI.BeginChangeCheck();
+
+            pixelmindGenerator.generatorOptionsIndex = EditorGUILayout.Popup(
+                pixelmindGenerator.generatorOptionsIndex,
+                pixelmindGenerator.generatorOptions
+            );
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                pixelmindGenerator.GetGeneratorFields(pixelmindGenerator.generatorOptionsIndex);
+            }
+
+            foreach (var field in pixelmindGenerator.generatorFields)
+            {
+                // Begin horizontal layout
+                EditorGUILayout.BeginHorizontal();
+                
+                var required = field.required ? "*" : "";
+                // Create label for field
+                EditorGUILayout.LabelField(field.key + required);
+
+                // Create text field for field value
+                field.value = EditorGUILayout.TextField(field.value);
+
+                // End horizontal layout
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+#endif
+}
